dcc8fdd9cee7943bdd974b5c4ce8680a7da61ce3
Chris Westbrook <chwestbr@microsoft.com>
2021-03-08 10:04:37 -0500

    finished basic browserhost sample

--- BrowserHost/Form1.vb
+++ BrowserHost/Form1.vb
@@ -2,7 +2,14 @@
 
 Public Class Form1
     Private Sub WebBrowser1_Navigating(sender As Object, e As WebBrowserNavigatingEventArgs) Handles WebBrowser1.Navigating
-        Debug.WriteLine(e.Url)
+        If (WebBrowser1.Document IsNot Nothing) Then
+            If (e.Url.ToString().EndsWith("#")) Then
+                Dim userName As String = WebBrowser1.Document.GetElementById("username").GetAttribute("value")
+                Dim pwd As String = WebBrowser1.Document.GetElementById("pwd").GetAttribute("value")
+                Debug.WriteLine(userName)
+                Debug.WriteLine(pwd)
+            End If
+        End If
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
